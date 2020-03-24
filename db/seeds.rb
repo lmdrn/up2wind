5fe5bbcc1a5723d5ef18311776c943d77ad226a7
Lea Medrano <leamedrano@gmail.com>
2020-03-24 16:38:41 +0100

    css produits eoliennes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ Eolienne.destroy_all
 
 puts "Creating eoliennes"
 
-file1 = URI.open('app/assets/images/eolienne2site.png')
-eolienne1 = Eolienne.new(name: 'Éolienne Pack Simple', price: '1000', description: 'Éolienne blanche simple')
-eolienne1.photo.attach(io: file1, filename: 'eolienne1.png', content_type:'image/png')
+file1 = URI.open('app/assets/images/Eolienne_City.jpg')
+eolienne1 = Eolienne.new(name: 'Éolienne Pack Avancé', price: '1500', description: 'Éolienne avec fond custom Up2wind')
+eolienne1.photo.attach(io: file1, filename: 'eolienne1.jpg', content_type:'image/jpg')
 eolienne1.save
 
-file2 = URI.open('app/assets/images/Eolienne_City.jpg')
-eolienne2 = Eolienne.new(name: 'Éolienne Pack Avancé', price: '1500', description: 'Éolienne avec fond custom Up2wind')
-eolienne2.photo.attach(io: file2, filename: 'eolienne2.jpg', content_type:'image/jpg')
+file2 = URI.open('app/assets/images/eolienne2site.png')
+eolienne2 = Eolienne.new(name: 'Éolienne Pack Simple', price: '1000', description: 'Éolienne blanche simple')
+eolienne2.photo.attach(io: file2, filename: 'eolienne2.png', content_type:'image/png')
 eolienne2.save
 
 file3 = URI.open('app/assets/images/Eolienne_mountain.jpg')
