be0511c8d99b230dcb438eb926c213faadc88c1b
Lea Medrano <leamedrano@gmail.com>
2020-03-27 10:30:03 +0100

    pages valeurs et fonctionnement

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def valeurs
+  end
+
+  def fonctionnement
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'eoliennes#index'
 
   resources :eoliennes, only: [ :index, :show ]
-
+  get '/valeurs', to: 'pages#valeurs'
+  get '/fonctionnement', to: 'pages#fonctionnement'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
